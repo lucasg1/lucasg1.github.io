5c1d5f3fd64d396f95c38ba00c72be33463250a4
lucasg1 <lucas97bg@gmail.com>
2019-03-14 21:09:17 -0300

    Funcionando com o processo rodando na maquina virtual

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.91.175:8545"));
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
 // Colocar o numero de contrato a cada novo processo
-contractInstance = VotingContract.at('0x0d4e163275564146de02fe0d57eda6b70bd975a9');
+contractInstance = VotingContract.at('0x17168a48b23a930809b748d2e944559062d3dd66');
 candidates = {"Germano": "candidato-1", "Carla": "candidato-2", "Jose": "candidato-3"}
 
 function voteForCandidate() {
